4c6d9fee3998a97368bd7b6f04d21fe55446e496
Minh Phung <mphung1@villanova.edu>
2022-11-28 01:51:08 -0500

    fix for deployment

--- components/ChatSupport.tsx
+++ components/ChatSupport.tsx
@@ -4,17 +4,12 @@ import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 const RouterChat = () => {
   const tawkMessengerRef = useRef<any>();
 
-  const handleMinimize = () => {
-    tawkMessengerRef.current.minimize();
-  };
-
   const onLoad = () => {
     console.log("onLoad works!");
   };
 
   return (
     <div>
-      <button onClick={handleMinimize}> Minimize the Chat </button>
       <TawkMessengerReact
         propertyId={process.env.NEXT_PUBLIC_TAWK_PROPERTY_ID}
         widgetId={process.env.NEXT_PUBLIC_TAWK_WIDGET_ID}
